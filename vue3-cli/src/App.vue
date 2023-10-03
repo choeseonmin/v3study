ee3d6aee0af86bc694e6dab12f0eab2924ebbf1f
choeseonmin <albamm520@naver.com>
2023-10-03 19:50:46 +0900

    컴포넌트 파일을 따로 만들어서 import 형식으로 컴포넌트 불러오기

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader></AppHeader>
+  <div>
+    {{ message }}
+  </div>
+  <button @click="showAlert">인사</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import AppHeader from './components/AppHeader.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+  AppHeader: AppHeader
+  },
+
+  data() {
+    return {
+    message: 'hi'
+    }
+  },
+  methods: {
+    showAlert() {
+      alert('hello')
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
